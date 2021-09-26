bd40e17efa79ee5931836312c4142d1631a0e43a
FiataliS <50484808+FiataliS@users.noreply.github.com>
2021-09-26 12:20:45 +0500

    Delete SearchOneAndFour.java

--- src/main/java/lesson_6/SearchOneAndFour.java
+++
@@ -1,9 +0,0 @@
-package lesson_6;
-
-public class SearchOneAndFour {
-
-    public static boolean search(int... arr) {
-        for (int i = 0; i < arr.length; i++) {if (arr[i] == 4 || arr[i]==1) {return true;}}
-        return false;
-    }
-}
